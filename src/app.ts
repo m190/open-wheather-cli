88312d5246a2bdb383df7244dc778e3d8eef688f
Alexandr Maximov <maks190@gmail.com>
2020-03-31 18:08:52 +0200

    Add yargs (args parsing)

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello world!");
+import yargs = require('yargs');
+
+enum Temp {
+    CELSIUS = "c",
+    FAHRENHEIT = "f"
+}
+
+const argv = yargs
+    .options({
+        "city": {
+            alias: ["c", "z"],
+            describe: "City or Zip"
+        },
+        "temp": {
+            alias: "t",
+            choices: [Temp.CELSIUS, Temp.FAHRENHEIT],
+            default: Temp.CELSIUS,
+            describe: "Temperature format"
+        },
+        "last": {
+            alias: "l",
+            describe: "Fetch weather for the latest query"
+        },
+        "import": {
+            alias: "i",
+            describe: "Import the config file"
+        }
+    })
+    .argv;
+
+console.log(argv);
